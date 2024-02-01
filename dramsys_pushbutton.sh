3abae60ff675fe242d45ad49689b87b909a75f02
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-02-01 14:51:23 +0100

    [CI] Update Python Dependency in CI

--- dramsys_pushbutton.sh
+++ dramsys_pushbutton.sh
@@ -2,6 +2,7 @@ source sourceme.sh
 make dramsys_redmule_preparation
 pip3 install -r core/requirements.txt
 pip3 install -r gapy/requirements.txt
+pip3 install -r requirements.txt
 make TARGETS=pulp-open-ddr all
 gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
 make configure -C pulp/pulp/redmule/run
