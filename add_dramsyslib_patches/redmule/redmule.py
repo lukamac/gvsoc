d1a3bae69fd6ef231395f539b46f42dbb818754b
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-02-02 17:38:48 +0100

    [REDMULE-v1.0] Add #BANK parameter for RedMule

--- add_dramsyslib_patches/redmule/redmule.py
+++ add_dramsyslib_patches/redmule/redmule.py
@@ -2,8 +2,12 @@ import gvsoc.systree as st
 
 class RedMule(st.Component):
 
-    def __init__(self, parent, name):
+    def __init__(self, parent, name, nb_tcdm_banks):
 
         super(RedMule, self).__init__(parent, name)
 
         self.set_component('pulp.redmule.redmule')
+
+        self.add_properties({
+            'nb_tcdm_banks': nb_tcdm_banks,
+        })
