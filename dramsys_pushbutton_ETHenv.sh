e6d696e88cc6a8008d371523472bd9c5582bfa20
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-04-05 16:51:07 +0200

    [SoftHier] add occamy pdk checking in push-button script

--- dramsys_pushbutton_ETHenv.sh
+++ dramsys_pushbutton_ETHenv.sh
@@ -8,3 +8,5 @@ CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=pulp-open-ddr all
 CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=occamy all
 ./install/bin/gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
 ./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary examples/occamy/multi_cluster.elf run
+make occamy_pdk_preparation
+./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary third_party/occamy/target/sim/sw/device/apps/blas/gemm/build/gemm.elf run
