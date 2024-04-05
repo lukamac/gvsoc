d9324834b01b952f97e64bb6e172d795515fb37c
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-04-05 15:41:26 +0200

    [CI] add CI test for occamy model

--- dramsys_pushbutton_ETHenv.sh
+++ dramsys_pushbutton_ETHenv.sh
@@ -5,4 +5,6 @@ pip3 install -r core/requirements.txt --user
 pip3 install -r gapy/requirements.txt --user
 pip3 install dataclasses --user
 CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=pulp-open-ddr all
+CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=occamy all
 ./install/bin/gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
+./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary examples/occamy/multi_cluster.elf run
