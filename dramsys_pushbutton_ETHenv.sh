b227b54034aa70f9a6ebd95ca65b5cd310fb847e
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-04-15 22:06:23 +0200

    [SoftHier] push-button: update

--- dramsys_pushbutton_ETHenv.sh
+++ dramsys_pushbutton_ETHenv.sh
@@ -10,4 +10,4 @@ CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=occamy all
 ./install/bin/gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
 ./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary examples/occamy/multi_cluster.elf run
 make occamy_pdk_preparation
-./install/bin/gvsoc --target=occamy --binary third_party/occamy/target/sim/sw/host/apps/offload/build/offload-test.elf --debug-binary third_party/occamy/target/sim/sw/device/apps/blas/test/build/test.elf run
+./install/bin/gvsoc --target=occamy --binary third_party/occamy/target/sim/sw/host/apps/offload/build/offload-test.elf --debug-binary third_party/occamy/target/sim/sw/device/apps/blas/test/build/test.elf run --trace=chip/soc/quadrant_0/cluster_0/redmule
