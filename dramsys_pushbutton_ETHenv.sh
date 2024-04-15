76adf731edbb8fa2167cbf0a0dae0e06944c8c60
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-04-15 18:11:00 +0200

    [SoftHier] push-button update

--- dramsys_pushbutton_ETHenv.sh
+++ dramsys_pushbutton_ETHenv.sh
@@ -8,8 +8,6 @@ pip3 install dataclasses --user
 CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=pulp-open-ddr all
 CXX=g++-11.2.0 CC=gcc-11.2.0 CMAKE=cmake-3.18.1 make TARGETS=occamy all
 ./install/bin/gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
-# ./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary examples/occamy/multi_cluster.elf run
-# make occamy_pdk_preparation
-# ./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary third_party/occamy/target/sim/sw/device/apps/blas/gemm/build/gemm.elf run
-# make snitch_pdk_preparation
-# ./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary third_party/snitch_cluster/target/snitch_cluster/sw/apps/redmule/build/redmule.elf run
+./install/bin/gvsoc --target=occamy --binary examples/occamy/offload-multi_cluster.elf --debug-binary examples/occamy/multi_cluster.elf run
+make occamy_pdk_preparation
+./install/bin/gvsoc --target=occamy --binary third_party/occamy/target/sim/sw/host/apps/offload/build/offload-test.elf --debug-binary third_party/occamy/target/sim/sw/device/apps/blas/test/build/test.elf run
